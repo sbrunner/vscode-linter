efbe5c03f55e0f824bf08f7f8b3b2c83d4dd78a4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-01-12 18:53:35 +0100

    Initial plugin version

---
+++ linter-prospector/src/extension.ts
@@ -0,0 +1,70 @@
+import {
+	LinterGetOffensesFunction,
+	LinterParseFixOutputFunction,
+	LinterOffense,
+	LinterOffenseSeverity,
+  } from "vscode-linter-api";
+  import { Uri } from "vscode";
+
+  interface ProspectorOffense {
+	messages: ProspectorMessage[];
+  }
+
+  interface ProspectorMessage {
+	source: string;
+	code: string;
+	location: ProspectorLocation;
+	message: string;
+  }
+
+  interface ProspectorLocation {
+	path: string;
+	module: string;
+	function: string;
+	line: number;
+	character: number;
+  }
+
+  const messageRegex = /(?<message>.*) \[See: (?<url>.*)\]/;
+
+  const offenseSeverity: { [key: string]: LinterOffenseSeverity } = {
+	convention: LinterOffenseSeverity.warning,
+	warning: LinterOffenseSeverity.error,
+  };
+
+  export const getOffenses: LinterGetOffensesFunction = ({ stdout, uri }: { stdout: string; uri: Uri }) => {
+	const result: ProspectorOffense = JSON.parse(stdout);
+	const offenses: LinterOffense[] = [];
+
+	result.messages.forEach((message: ProspectorMessage) => {
+	  let messageString = message.message;
+	  let docsUrl = "";
+	  const match = message.message.match(messageRegex);
+	  if (match) {
+		messageString = match.groups!.message;
+		docsUrl = match.groups!.url;
+	  }
+
+	  offenses.push({
+		uri,
+		lineStart: Math.max(0, message.location.line - 1),
+		columnStart: Math.max(0, message.location.character - 1),
+		lineEnd: Math.max(0, message.location.line - 1),
+		columnEnd: Math.max(0, message.location.character),
+		code: `${message.source}:${message.code}`,
+		message: messageString,
+		severity: LinterOffenseSeverity.error,
+		source: "prospector",
+		correctable: false,
+		docsUrl: docsUrl,
+		endLine: number;
+		endColumn: number;
+       fix?: {
+	  });
+	});
+
+	return Promise.resolve(offenses);
+  };
+
+  export const parseFixOutput: LinterParseFixOutputFunction = ({ stdout }: { stdout: string }) =>
+	Promise.resolve(stdout);
